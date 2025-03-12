799f259f8726c308573cf7db718bac682501ee17
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-12 21:52:09 +0530

    update files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,6 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-export default App*/
  
 import React from 'react'
-import { Route,Routes }  from 'react-router-dom'
+import { Route, Routes }  from 'react-router-dom'
 import Home from './pages/Home'
 import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
@@ -43,9 +9,9 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/apply-job/:id' element={<ApplyJob/>}/>
-        <Route path='/applications' element={<Applications/>}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/apply-job/:id' element={<ApplyJob />}/>
+        <Route path='/applications' element={<Applications />}/>
       </Routes>
     </div>
   )
