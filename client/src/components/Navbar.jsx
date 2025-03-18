df314e9eeea32dab54a0bc3ef8552c0987dc7e57
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-18 22:24:22 +0530

    improved user name change option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { useClerk, userButton, useUser } from '@clerk/clerk-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
@@ -10,14 +10,14 @@ const Navbar = () => {
 
   return (
     <div className='shadow py-4'>
-        <div className='container px-4 2x1:px-20 mx--auto flex justify-between item-center'>
+        <div className='container px-4 2x1:px-20 mx--auto flex justify-between items-center'>
             <img src={assets.logo} alt="" />
             <div className='flex gap-4 max-sm:text-xs'>
                 <button className='text-gray-600'>Recruiter Login</button>
-                <button ocClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
+                <button onClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
             </div>
         </div>
-    </div>
+    </div> 
   )
 }
 
