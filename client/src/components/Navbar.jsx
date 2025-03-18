98d31794b6e7f9b6cfc551dcae29c39e9c054ece
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-18 22:15:32 +0530

    Navbar.jsx update to sign in option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useClerk, userButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
+
+    const {openSignIn} = useClerk()
+
+
+
   return (
     <div className='shadow py-4'>
         <div className='container px-4 2x1:px-20 mx--auto flex justify-between item-center'>
             <img src={assets.logo} alt="" />
             <div className='flex gap-4 max-sm:text-xs'>
                 <button className='text-gray-600'>Recruiter Login</button>
-                <button className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
+                <button ocClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
             </div>
         </div>
     </div>
   )
 }
 
-export default Navbar                 25.33
+export default Navbar
