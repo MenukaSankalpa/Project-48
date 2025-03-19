5cacc61c43d57f3f0d475a5e084214ac63eed401
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-19 22:38:27 +0530

    improve when user login login icons hide option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 const Navbar = () => {
 
     const {openSignIn} = useClerk()
+    const {user} = useUser()
 
 
 
@@ -12,10 +13,17 @@ const Navbar = () => {
     <div className='shadow py-4'>
         <div className='container px-4 2x1:px-20 mx--auto flex justify-between items-center'>
             <img src={assets.logo} alt="" />
-            <div className='flex gap-4 max-sm:text-xs'>
-                <button className='text-gray-600'>Recruiter Login</button>
-                <button onClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
+            {
+              user
+              ?<div>
+                
+              </div>
+              :<div className='flex gap-4 max-sm:text-xs'>
+              <button className='text-gray-600'>Recruiter Login</button>
+              <button onClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
             </div>
+            } 
+            
         </div>
     </div> 
   )
