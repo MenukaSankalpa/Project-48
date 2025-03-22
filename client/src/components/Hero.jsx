955dbfc4ff1f6b806dee8107f227cf7d757232fb
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-22 19:32:57 +0530

    css add div1,2,h2,p,div3

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets } from '../assets/assets'  
 
 const Hero = () => {
   return (
-    <div>
-        <div>
-            <h2>Over 10,000+ jobs to apply</h2>
-            <p>Your Next Big Career Move Start Right Here-Explore the best job opportunity and take the first step toward your future!</p>
-            <div>
-                <div>
+    <div className='container 2xl:px-20 mx-auto my-10'>
+        <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl' >
+            <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
+            <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5' >Your Next Big Career Move Start Right Here-Explore the best job opportunity and take the first step toward your future!</p>
+            <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto'> 
+                <div className='flex item-center '>
                     <img src={assets.search_icon} alt="" />
                     <input type="text"  placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full'/>
                 </div>
+                <div className='flex item-center ' >
+                    <img src={assets.location_icon} alt="" />
+                    <input type="text"  placeholder='location_icon' className='max-sm:text-xs p-2 rounded outline-none w-full'/>
+                </div>
+                <button>Search</button> 
             </div>
         </div>
     </div>
