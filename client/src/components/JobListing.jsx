fc6799f298217377294c73520ae62d39e9776363
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-25 15:14:06 +0530

    filtering search done

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/appContext'
 import { assets, JobCategories, JobLocations, } from '../assets/assets'
 import JobCard from './JobCard'
@@ -24,6 +24,26 @@ const JobListing = () => {
             prev => prev.includes(location) ? prev.filter( c => c !== location) : [...prev,location]
         )
     }
+
+    useEffect (()=>{
+
+        const matchesCategory = job => selectedCategories.length === 0 || selectedCategories.includes(job.category)
+
+        const matchesLocation = job => selectedLocations.length === 0 || selectedLocations.includes(job.location)
+
+        const matchesTitle = job => searchFilter.title === "" || job.title.toLowerCase().includes(searchFilter.title.toLowerCase())
+
+        const matchesSearchLocation = job =>searchFilter.location === "" ||  job.location.toLowerCase().includes(searchFilter.location.toLowerCase())
+
+        const newFilteredJobs = jobs.slice().reverse().filter(
+            job => matchesCategory(job) && matchesLocation(job) && matchesTitle(job) && matchesSearchLocation(job)
+
+        )
+
+        setFilteredJobs(newFilteredJobs)
+        setCurrentPage(1)
+
+    },[jobs,selectedCategories,selectedLocations,searchFilter])
 
   return (
     <div className='container 2x1:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
@@ -98,24 +118,24 @@ const JobListing = () => {
             <h3 className='font-medium text-3xl py-2' id='job-list'>Latest Job</h3>
             <p className='mb-8'>get your desired job from top companies</p>
             <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
-                {jobs.slice((currentPage-1)*6,currentPage*6).map((job,index)=>(
+                {filteredJobs.slice((currentPage-1)*6,currentPage*6).map((job,index)=>(
                     <JobCard key={index} job={job} />
                 ))}
             </div>
 
             {/* pagination */}
-            {jobs.length > 0 && (
+            {filteredJobs.length > 0 && (
                 <div className='flex items-center justify-center space-x-2 mt-10'>
                     <a href="#job-list">
                         <img  onClick={() => setCurrentPage(Math.max(currentPage-1),1)} src={assets.left_arrow_icon} alt="" />
                     </a>
-                    {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index) => (
+                    {Array.from({length:Math.ceil(filteredJobs.length/6)}).map((_,index) => (
                         <a href="#job-list">
                             <button onClick={()=> setCurrentPage(index+1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage === index + 1 ? 'bg-blue-100 text-blue-500' : 'text-gray-500'}`} >{index + 1}</button>
                         </a>
                     ))}
                     <a href="#job-list">
-                        <img onClick={() => setCurrentPage(Math.min(currentPage+1,Math.ceil(jobs.length / 6)))} src={assets.right_arrow_icon} alt="" />
+                        <img onClick={() => setCurrentPage(Math.min(currentPage+1,Math.ceil(filteredJobs.length / 6)))} src={assets.right_arrow_icon} alt="" />
                     </a>
                 </div>
             )}
