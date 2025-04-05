edb2c71b9b81d41415b1bbb8e6b628dc50973570
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-05 22:27:24 +0530

    updated

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { assets, viewApplicationsPageData } from '../assets/assets'
 
 const ViewApplications = () => {
   return (
-    <div>ViewApplications</div>
-  )
+    <div>
+      <div>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>User Name</th>
+            <th>Job Title</th>
+            <th>Location</th>
+            <th>Resume</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {viewApplicationsPageData.map((applicant, index)=>(
+            <tr>
+              <td>{index+1}</td>
+              <td>
+                <img src={applicant.imgSrc} alt="" />
+                <span>{applicant.name}</span>
+              </td>
+              <td>{applicant.jobTitle}</td>
+              <td>{applicant.location}</td>
+              <td>
+                <a href="" target='_blank'>
+                  Resume <img src={assets.resume_download_icon} alt="" />
+                </a>
+              </td>
+              <td>
+                <div>
+                  
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </div>
+    </div>
+  ) 
 }
 
 export default ViewApplications
