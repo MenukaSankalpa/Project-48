1cccda17b8fc236e09536c2baaaf0f0298823a85
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-06 22:38:36 +0530

    managejob frontend done

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { manageJobsData } from '../assets/assets'
+import moment from 'moment'
 
 const ManageJobs = () => {
   return (
-    <div>ManageJobs</div>
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Job Tittle</th>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Applicants</th>
+              <th>Visible</th>
+            </tr>
+          </thead>
+          <tbody>
+            {manageJobsData.map((job,index)=>(
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>{job.title}</td>
+                <td>{moment(job.date).format('ll')}</td>
+                <td>{job.location}</td>
+                <td>{job.applicants}</td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
