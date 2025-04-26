da4da99610153dcb8e14f1fb7052c2a4926dda75
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-26 09:45:31 +0530

    update server files all files

--- client/src/pages/app.js
+++
@@ -1,9 +0,0 @@
-import { ClerkProvider } from "@clerk/clerk-react";
-
-export default function App({ Component, pageProps }) {
-    return (
-        <ClerkProvider>
-            <Component {...pageProps} />
-        </ClerkProvider>
-    );
-}
