4ed013d2dc54edafb218483631645467b256ff33
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-07-11 22:49:28 +0530

    register company function make

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,6 +1,34 @@
+import Company from "../models/Company";
+import bcrypt from 'bcrypt'
+
+
 
 //register a new company 
 export const registerCompany = async (req,res) => {
+
+    const {name, email, password} = req.body
+
+    const imageFile = req.file;
+
+    if (!name || !email || !password || !imageFile) {
+        return res.json({success:false, message: "Missing Details"})
+    }
+
+    try {
+        const companyExists = await Company.findOne({email})
+
+        if (companyExists) {
+            return res.json({success:false, message: 'Company already registered'})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
+
+        
+
+    } catch (error) {
+        
+    }
 
 }
 
